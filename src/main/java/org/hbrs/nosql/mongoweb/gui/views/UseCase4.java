35c900a62c7731625dd777a177818114843675da
Sasorien <ja.mu4u@web.de>
2016-12-20 16:01:00 +0100

    UseCase Klassen angelegt

---
+++ src/main/java/org/hbrs/nosql/mongoweb/gui/views/UseCase4.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.hbrs.nosql.mongoweb.gui.views;
+
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
+import org.hbrs.nosql.mongoweb.gui.components.TopPanel;
+import org.hbrs.nosql.mongoweb.model.UseCase;
+
+/**
+ * @author Franz
+ */
+public class UseCase4 extends VerticalLayout implements View
+{
+    @Override
+    public void enter( ViewChangeListener.ViewChangeEvent viewChangeEvent )
+    {
+        setUp();
+    }
+    
+    private void setUp()
+    {
+        this.addComponent( new TopPanel() );
+        
+        Label title = new Label( UseCase.UC4.name() );
+        this.addComponent( title );
+        
+        setMargin( true );
+        setSpacing( true );
+    }
+}
--- src/main/java/org/hbrs/nosql/mongoweb/model/UseCase.java
+++ src/main/java/org/hbrs/nosql/mongoweb/model/UseCase.java
@@ -1,7 +1,6 @@
 package org.hbrs.nosql.mongoweb.model;
 
-import org.hbrs.nosql.mongoweb.gui.views.UseCase1;
-import org.hbrs.nosql.mongoweb.gui.views.UseCase2;
+import org.hbrs.nosql.mongoweb.gui.views.*;
 
 /**
  * Created by Jan Eric Müller (aka Saso) on 20.12.2016.
@@ -10,11 +9,11 @@ public enum UseCase
 {
     UC1 ( "Use Case 1", UseCase1.class ),
     UC2 ( "Use Case 2", UseCase2.class ),
-    UC3 ( "Use Case 3", UseCase1.class ),
-    UC4 ( "Use Case 4", UseCase1.class ),
-    UC5 ( "Use Case 5", UseCase1.class ),
-    UC6 ( "Use Case 6", UseCase1.class ),
-    UC7 ( "Use Case 7", UseCase1.class ),
+    UC3 ( "Use Case 3", UseCase3.class ),
+    UC4 ( "Use Case 4", UseCase4.class ),
+    UC5 ( "Use Case 5", UseCase5.class ),
+    UC6 ( "Use Case 6", UseCase6.class ),
+    UC7 ( "Use Case 7", UseCase7.class ),
     ;
     
     private String name;
