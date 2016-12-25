70c4268f44a638e99847a5c80e42b02767004e78
Franz <fddahmann91@gmail.com>
2016-12-25 15:55:00 +0100

    usecases Backend fertiggestellt.

--- src/main/java/org/hbrs/nosql/mongoweb/db/MongoDBConnector.java
+++ src/main/java/org/hbrs/nosql/mongoweb/db/MongoDBConnector.java
@@ -10,6 +10,7 @@ package org.hbrs.nosql.mongoweb.db;
  * @author Franz Dahmann
  */
 
+import com.mongodb.AggregationOutput;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.MongoClient;
@@ -117,6 +118,64 @@ public class MongoDBConnector
         BasicDBObject orquery = new BasicDBObject( "$or", obj );
         
         List answer = coll.distinct( "1_Pruefer", orquery );
+        return answer;
+    }
+    public List getQuery3( String input )
+    {
+        Pattern pat = Pattern.compile( input, Pattern.CASE_INSENSITIVE );
+        BasicDBObject query;
+        query = new BasicDBObject( "1_Pruefer", pat );
+        List answer = coll.distinct( "Thema_der_Arbeit", query );
+        return answer;
+    }
+
+    public int getQuery4(String input){
+        List< BasicDBObject > obj = new ArrayList< BasicDBObject >();
+        obj.add( new BasicDBObject( "Semester", input ) );
+        obj.add( new BasicDBObject( "Zeugnis", 1 ) );
+        BasicDBObject andQuery = new BasicDBObject("$and",obj);
+        int i = coll.find(andQuery).count();
+        return i;
+    }
+    public List getQuery5(){
+        BasicDBObject nequery = new BasicDBObject( "$ne", "" );
+        BasicDBObject query = new BasicDBObject("Firma", nequery);
+        List answer = coll.distinct("Firma",query);
+        //db.abDBsimpleCSV.distinct('Firma',{Firma: {$ne:""}})
+        return answer;
+    } 
+    public AggregationOutput getQuery6(){
+        List< BasicDBObject > obj = new ArrayList< BasicDBObject >();
+        //Erstes Element der Queryliste
+        BasicDBObject sizeQuery = new BasicDBObject("$size","0");
+        BasicDBObject neQuery = new BasicDBObject("$not",sizeQuery);
+        BasicDBObject matchQuery = new BasicDBObject("1_Pruefer",neQuery);
+        obj.add( new BasicDBObject( "$match", matchQuery ) );
+        //Zweites Element der Queryliste
+        obj.add( new BasicDBObject("$unwind","1_Pruefer"));
+        //Drittes Element der Queryliste
+        BasicDBObject sumQuery = new BasicDBObject("$sum",1);
+        DBObject auswahlQuery = new BasicDBObject("_id","1_Pruefer")
+                .append("Anzahl", sumQuery);
+        obj.add( new BasicDBObject("$group",auswahlQuery));
+        //Viertes Element der Queryliste
+        BasicDBObject anzahlQuery, greaterQuery;
+        greaterQuery = new BasicDBObject("$gte",2);
+        anzahlQuery = new BasicDBObject("Anzahl",greaterQuery);
+        obj.add( new BasicDBObject("$match",anzahlQuery));
+        //Fünftes Element der Queryliste
+        BasicDBObject sortQuery = new BasicDBObject("Anzahl",-1);
+        obj.add( new BasicDBObject("$match",sortQuery));
+        //Sechstes Element der Queryliste
+        obj.add( new BasicDBObject("$limit",200));
+        AggregationOutput answer = coll.aggregate(obj);
+        return answer;
+    }
+    public List getQuery7(String input){
+        Pattern pat = Pattern.compile( input, Pattern.CASE_INSENSITIVE );
+        BasicDBObject query;
+        query = new BasicDBObject( "Thema_der_Arbeit", pat );
+        List answer = coll.distinct( "Thema_der_Arbeit", query );
         return answer;
     }
 }
